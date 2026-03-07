1158fa844d4b8aba53f62b12c5ee540de44b50b3
Dea <tohkiee@protonmail.com>
2026-03-07 12:07:16 -0500

    cleanup of program order

--- groups/yezelhlev/programs.nix
+++ groups/yezelhlev/programs.nix
@@ -10,56 +10,59 @@
   # ======================
   # sudo nixos-rebuild switch --flake .
   environment.systemPackages = with upkgs; [
+    # CLI tools
+    git
     wget
+    unzip
+    p7zip
+    kitty # terminal
+    starship # shell customizer
+    btop-cuda # system monitoring
+    croc # e2ee file transfer
+    chawan # tui browser cha <url>
+    shellcheck # sh warning
+
+    # editors
     lunarvim
     vscode
-    python3
-    p7zip
-    unzip
-    chawan # tui browser cha <url>
-    git
+
+    # languages
     gcc
     gdb
     glibc.static
+    python3
     nixfmt
 
-    kitty # terminal
+    # window management
     wayland
 
-    nemo # file browser
-    monophony # yt music tryout
-
+    # programs 
+    vivaldi # vivaldi install requires qt5
+    zen # TODO: switch to better browser
     vesktop # discord-canary
     equibop # equibop 3.1.7
-
-    vivaldi # vivaldi install requires qt5
-    qt5.qtbase
-
-    starship # shell customizer
-
+    element-desktop # element 4 matrix
+    monophony # yt music tryout
+    syncplay
+    qbittorrent
     libreoffice-qt # libreoffice
-    hunspell
+    notepad-next # notepad++
+    # notepadqq # TODO: which is better?
+    sqlitebrowser
+    bottles # windows emulation
+
+    # dependencies 
+    qt5.qtbase # vivaldi + others
+    libsForQt5.qtstyleplugin-kvantum # libs for Qt5
+    libsForQt5.qt5ct
+    libsForQt5.qt5.qtbase
+
+    hunspell # spellcheck for libreoffice
     hunspellDicts.uk_UA
     hunspellDicts.th_TH
     openjdk
 
-    notepad-next # notepad++
-    sqlitebrowser
-
-    ffmpeg-full
+    ffmpeg-full # mpv + syncplay
     yt-dlp
-    qbittorrent
-
-    bottles # windows emulation
-
-    element-desktop # element 4 matrix
-
-    libsForQt5.qtstyleplugin-kvantum # libs for Qt5
-    libsForQt5.qt5ct
-    libsForQt5.qt5.qtbase
-    btop-cuda
-
-    croc
-    syncplay
   ];
 }
