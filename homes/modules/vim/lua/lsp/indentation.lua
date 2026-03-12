586e5832ffdda5d7e8257012a78bd1b1c94f2844
Dea <tohkiee@protonmail.com>
2026-03-12 14:45:31 -0400

    haskell indentation

--- homes/modules/vim/lua/lsp/indentation.lua
+++ homes/modules/vim/lua/lsp/indentation.lua
@@ -8,6 +8,15 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
+  pattern = "hs",
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.expandtab = true
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
   pattern = "nix",
   callback = function()
     vim.opt_local.shiftwidth = 2
